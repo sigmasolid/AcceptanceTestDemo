a97ef61abb7b70ba14bd241909ff5464f133c56e
Steffen <dk.steffen@gmail.com>
2024-01-12 10:47:41 +0100

    Initial creation of project

---
+++ AcceptanceTestDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// Create builder for minimal API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Create app
+var app = builder.Build();
+
+// Setup routes
+app.MapGet("/", () => "Hello World!");
+
+// Start app
+app.Run();
