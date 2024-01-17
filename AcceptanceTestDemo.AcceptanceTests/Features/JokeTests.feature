acc4a25d05495d18a0934c50081a953f1d116460
Steffen <dk.steffen@gmail.com>
2024-01-17 21:45:13 +0100

    Set up naive feature and steps

---
+++ AcceptanceTestDemo.AcceptanceTests/Features/JokeTests.feature
@@ -0,0 +1,7 @@
+﻿Feature: JokeTests
+	Simple tests of the Dad Joke API
+
+Scenario: Get a random joke
+	Given a joke already exists
+	When the endpoint for a random joke is called
+	Then a joke should be returned
---
+++ AcceptanceTestDemo.AcceptanceTests/Steps/JokeStepDefinitions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTestDemo.AcceptanceTests.Steps;
+
+[Binding]
+public sealed class JokeStepDefinitions
+{
+    // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+    private readonly ScenarioContext _scenarioContext;
+
+    public JokeStepDefinitions(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [Given(@"a joke already exists")]
+    public void GivenAJokeAlreadyExists()
+    {
+    }
+
+    [When(@"the endpoint for a random joke is called")]
+    public void WhenTheEndpointForARandomJokeIsCalled()
+    {
+    }
+
+    [Then(@"a joke should be returned")]
+    public void ThenAJokeShouldBeReturned()
+    {
+    }
+}
