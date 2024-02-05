e6bf3ab69a74eb5700d1ea69b6f6a01d7a3a2cfe
Steffen <dk.steffen@gmail.com>
2024-02-05 20:07:32 +0100

    Working container example

--- DadJoke.WebApi.AcceptanceTests/Hooks/ApplicationHooks.cs
+++ DadJoke.WebApi.AcceptanceTests/Hooks/ApplicationHooks.cs
@@ -1,6 +1,9 @@
-﻿using DadJoke.WebApi.Controllers;
+﻿using System.Net;
+using DadJoke.WebApi.Controllers;
 using BoDi;
 using DadJoke.Domain.Services;
+using Ductus.FluentDocker.Builders;
+using Ductus.FluentDocker.Services;
 using TechTalk.SpecFlow;
 
 namespace DadJoke.WebApi.AcceptanceTests.Hooks;
@@ -8,11 +11,24 @@ namespace DadJoke.WebApi.AcceptanceTests.Hooks;
 [Binding]
 public class ApplicationHooks(ObjectContainer objectContainer)
 {
+    private static ICompositeService _compositeService = null!;
+    private const string DockerComposeFileName = "docker-compose.yml";
+    private const string BaseAddress = "http://localhost:8080";
     
     [BeforeTestRun]
-    public static void BeforeTest()
+    public static void DockerComposeUp()
     {
-        
+        var dockerComposePath = GetDockerComposeLocation();
+
+        var confirmationUrl = BaseAddress;
+        _compositeService = new Builder()
+            .UseContainer()
+            .UseCompose()
+            .FromFile(dockerComposePath)
+            .RemoveOrphans()
+            .WaitForHttp("dadjoke.webapi", $"{confirmationUrl}/",
+                continuation: (response, _) => response.Code != HttpStatusCode.OK ? 2000 : 0)
+            .Build().Start();
     }
 
     [BeforeScenario]
@@ -20,12 +36,30 @@ public class ApplicationHooks(ObjectContainer objectContainer)
     {
         var httpClient = new HttpClient()
         {
-            BaseAddress = new Uri("http://localhost:5000")
+            BaseAddress = new Uri(BaseAddress)
         };
         // ObjectContainer is a dependency injection container provided by SpecFlow
         objectContainer.RegisterInstanceAs(httpClient);
         var dadJokeService = new DadJokeService(new InMemoryDadJokeRepository());
         var homeController = new HomeController(dadJokeService);
         objectContainer.RegisterInstanceAs(homeController);
+    }
+
+    [AfterTestRun]
+    public static void DockerComposeDown()
+    {
+        _compositeService.Stop();
+        _compositeService.Dispose();
+    }
+
+    private static string GetDockerComposeLocation()
+    {
+        var directory = Directory.GetCurrentDirectory();
+        while (!Directory.EnumerateFiles(directory, "*.yml").Any(s => s.EndsWith(DockerComposeFileName)))
+        {
+            directory = directory.Substring(0, directory.LastIndexOf(Path.DirectorySeparatorChar));
+        }
+
+        return Path.Combine(directory, DockerComposeFileName);
     }
 }
